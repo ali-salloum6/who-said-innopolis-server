aa2b86c30ce1eb3937ec09b8afa1236b52d03fb5
Ali <ali.salloum.gh@gmail.com>
2023-07-28 11:55:16 +0300

    made the check for the previous day

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ async def get_channel_messages():
                 entity, filter=InputMessagesFilterEmpty())
 
             # Get today's date
-            today = datetime.now().date()
+            today = datetime.now().date() - timedelta(days=1)
 
             async for message in messages:
                 # Extract only the date portion of the message date
